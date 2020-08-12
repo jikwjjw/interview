123a1c131bac56916921fae8d911e740cfd9850b
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-12 11:46:27 +0800

    业务接口

---
+++ 代理/CGLib动态代理/SayHelloImpl.java
@@ -0,0 +1,18 @@
+package CgLib;
+
+/**
+ *    <dependency>
+ *             <groupId>cglib</groupId>
+ *             <artifactId>cglib</artifactId>
+ *             <version>3.1</version>
+ *         </dependency>
+ */
+
+/**
+ * 业务类
+ */
+public class SayHelloImpl {
+    public void say() {
+        System.out.println("Hello World");
+    }
+}
